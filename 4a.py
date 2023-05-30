b0c682ce7a16da9734a1ad38569c93ad5febaaca
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-05-30 19:10:37 +0530

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,12 @@
+r=int(input("enter the value of r:"))
+a=0
+b=1
+while b<r:
+     c=a+b
+     a=b
+     b=c
+     
+if r==c:
+    print("the value is a fibonacci")
+else:
+    print("the value is not a fibonacci")
